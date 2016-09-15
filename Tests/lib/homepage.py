71d007d6d3609dc6db17762fd9e256d0a6fc54d0
Sanchari <sancharighoshroy@RRBLR161.local>
2016-09-15 16:05:11 +0530

    Added category selection methods

--- Tests/lib/homepage.py
+++ Tests/lib/homepage.py
@@ -9,8 +9,13 @@ class Homepage(BasePage):
 
     def select_category(self):
         self.driver.find_element_by_css_selector("#nav-link-shopall").click()
-        category = self.driver.find_element_by_css_selector('.nav_a[href="/Laptops/b/ref=sd_allcat_computers_laptop?ie=UTF8&node=1375424031"]')
+        category = self.driver.find_element_by_css_selector\
+            ('.nav_a[href="/Laptops/b/ref=sd_allcat_computers_laptop?ie=UTF8&node=1375424031"]')
         category.click()
+
+    def select_category_name(self):
+        category_name = self.driver.find_element_by_xpath('//img[@alt="Laptops for business and work"]')
+        category_name.click()
 
 
 
--- Tests/test_01_homepage_tests.py
+++ Tests/test_01_homepage_tests.py
@@ -1,11 +1,18 @@
 import unittest
 from selenium import webdriver
+from Tests.lib.homepage import Homepage
 
 
 class HomepageTests(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Chrome()
         self.browser.maximize_window()
+
+    def test_select_category(self):
+        category = Homepage(self.browser)
+        category.select_category()
+        category.select_category_name()
+
 
 
 
